2f2ca27b945e16d7d1ee9ca94d6a3967dfb94335
Jonathan Dean <ke4ukz@gmx.com>
2015-09-04 20:18:51 -0400

    Initial commit

---
+++ WindowWatcher/frmMain.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.lsvWindows = New System.Windows.Forms.ListView()
+        Me.chPID = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.chTitle = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.chProcessName = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.chCommandLine = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.tsbMonitoring = New System.Windows.Forms.ToolStripButton()
+        Me.tsbExportList = New System.Windows.Forms.ToolStripButton()
+        Me.tsbClearList = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 250
+        '
+        'lsvWindows
+        '
+        Me.lsvWindows.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.chPID, Me.chTitle, Me.chProcessName, Me.chCommandLine})
+        Me.lsvWindows.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lsvWindows.Location = New System.Drawing.Point(0, 25)
+        Me.lsvWindows.MultiSelect = False
+        Me.lsvWindows.Name = "lsvWindows"
+        Me.lsvWindows.Size = New System.Drawing.Size(508, 237)
+        Me.lsvWindows.TabIndex = 0
+        Me.lsvWindows.UseCompatibleStateImageBehavior = False
+        Me.lsvWindows.View = System.Windows.Forms.View.Details
+        '
+        'chPID
+        '
+        Me.chPID.Text = "PID"
+        '
+        'chTitle
+        '
+        Me.chTitle.Text = "Title"
+        '
+        'chProcessName
+        '
+        Me.chProcessName.Text = "ProcessName"
+        '
+        'chCommandLine
+        '
+        Me.chCommandLine.Text = "Command Line"
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsbMonitoring, Me.tsbExportList, Me.tsbClearList})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(508, 25)
+        Me.ToolStrip1.TabIndex = 1
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'tsbMonitoring
+        '
+        Me.tsbMonitoring.Checked = True
+        Me.tsbMonitoring.CheckOnClick = True
+        Me.tsbMonitoring.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.tsbMonitoring.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.tsbMonitoring.Image = CType(resources.GetObject("tsbMonitoring.Image"), System.Drawing.Image)
+        Me.tsbMonitoring.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbMonitoring.Name = "tsbMonitoring"
+        Me.tsbMonitoring.Size = New System.Drawing.Size(71, 22)
+        Me.tsbMonitoring.Text = "&Monitoring"
+        '
+        'tsbExportList
+        '
+        Me.tsbExportList.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.tsbExportList.Image = CType(resources.GetObject("tsbExportList.Image"), System.Drawing.Image)
+        Me.tsbExportList.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbExportList.Name = "tsbExportList"
+        Me.tsbExportList.Size = New System.Drawing.Size(65, 22)
+        Me.tsbExportList.Text = "&Export List"
+        Me.tsbExportList.ToolTipText = "Export the list to a text, CSV, or XML file"
+        '
+        'tsbClearList
+        '
+        Me.tsbClearList.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.tsbClearList.Image = CType(resources.GetObject("tsbClearList.Image"), System.Drawing.Image)
+        Me.tsbClearList.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbClearList.Name = "tsbClearList"
+        Me.tsbClearList.Size = New System.Drawing.Size(59, 22)
+        Me.tsbClearList.Text = "&Clear List"
+        Me.tsbClearList.ToolTipText = "Clears everything in the list"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(508, 262)
+        Me.Controls.Add(Me.lsvWindows)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Name = "frmMain"
+        Me.Text = "WindowWatcher"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents lsvWindows As System.Windows.Forms.ListView
+    Friend WithEvents chPID As System.Windows.Forms.ColumnHeader
+    Friend WithEvents chTitle As System.Windows.Forms.ColumnHeader
+    Friend WithEvents chProcessName As System.Windows.Forms.ColumnHeader
+    Friend WithEvents chCommandLine As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ToolStrip1 As System.Windows.Forms.ToolStrip
+    Friend WithEvents tsbExportList As System.Windows.Forms.ToolStripButton
+    Friend WithEvents tsbClearList As System.Windows.Forms.ToolStripButton
+    Friend WithEvents tsbMonitoring As System.Windows.Forms.ToolStripButton
+
+End Class
---
+++ WindowWatcher/frmMain.vb
@@ -0,0 +1,152 @@
+﻿Imports System.Xml
+
+Public Class frmMain
+
+    Private Declare Function GetForegroundWindow Lib "user32.dll" () As IntPtr
+    Private Declare Function GetWindowThreadProcessId Lib "user32.dll" (ByVal hwnd As IntPtr, ByRef lpdwProcessID As Integer) As Integer
+    Private Declare Function GetWindowText Lib "user32.dll" Alias "GetWindowTextA" (ByVal hWnd As IntPtr, ByVal WinTitle As String, ByVal MaxLength As Integer) As Integer
+    Private Declare Function GetWindowTextLength Lib "user32.dll" Alias "GetWindowTextLengthA" (ByVal hwnd As Integer) As Integer
+
+    Private LastTophWnd As Integer = 0
+    Private LastWindowTitle As String = ""
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim hWnd As IntPtr = GetForegroundWindow()
+        If hWnd = IntPtr.Zero Then Exit Sub
+
+        Dim TitleLength As Integer
+        TitleLength = GetWindowTextLength(hWnd.ToInt32)
+
+        Dim WindowTitle As String = StrDup(TitleLength + 1, " "c)
+        GetWindowText(hWnd, WindowTitle, TitleLength + 1)
+        WindowTitle = WindowTitle.Substring(0, TitleLength)
+
+        If (LastTophWnd = hWnd.ToInt32) AndAlso (LastWindowTitle = WindowTitle) Then
+            Exit Sub
+        Else
+            LastTophWnd = hWnd.ToInt32
+            LastWindowTitle = WindowTitle
+        End If
+
+        Dim PID As Integer = 0
+        GetWindowThreadProcessId(hWnd, PID)
+        If PID = 0 Then Exit Sub
+
+        Dim ProcessInfo As Process = Process.GetProcessById(PID)
+        If ProcessInfo Is Nothing Then Exit Sub
+
+        Dim LI As New ListViewItem
+        LI.Text = PID.ToString
+        LI.SubItems.Add(WindowTitle & " - " & ProcessInfo.MainWindowTitle)
+        LI.SubItems.Add(ProcessInfo.ProcessName)
+        LI.SubItems.Add(ProcessInfo.StartInfo.FileName & " " & ProcessInfo.StartInfo.Arguments)
+        lsvWindows.EnsureVisible(lsvWindows.Items.Add(LI).Index)
+    End Sub
+
+    Private Sub tsbClearList_Click(sender As Object, e As EventArgs) Handles tsbClearList.Click
+        Timer1.Enabled = False
+        LastTophWnd = 0
+        LastWindowTitle = ""
+        lsvWindows.Items.Clear()
+        Timer1.Enabled = tsbMonitoring.Checked
+    End Sub
+
+    Private Sub tsbExportList_Click(sender As Object, e As EventArgs) Handles tsbExportList.Click
+        Using SFD As New SaveFileDialog
+            With SFD
+                .AddExtension = True
+                .AutoUpgradeEnabled = True
+                .CheckFileExists = False
+                .CheckPathExists = True
+                .CreatePrompt = False
+                .DefaultExt = "txt"
+                .DereferenceLinks = True
+                .Filter = "Text Files|*.txt|CSV Files|*.csv|XML Files|*.xml|All Files|*.*"
+                .OverwritePrompt = True
+                .ShowHelp = False
+                .SupportMultiDottedExtensions = True
+                .Title = "Export List"
+                .ValidateNames = True
+            End With
+            If SFD.ShowDialog(Me) = Windows.Forms.DialogResult.OK Then
+                ExportItems(SFD.FileName, SFD.FilterIndex)
+            End If
+        End Using
+
+
+
+    End Sub
+
+    Private Sub ExportItems(ByVal Filename As String, ByVal FileType As Integer)
+        Dim OutFile As System.IO.StreamWriter
+        Dim OutLine As New System.Text.StringBuilder
+        Try
+            OutFile = My.Computer.FileSystem.OpenTextFileWriter(Filename, False)
+        Catch ex As Exception
+            MsgBox("Error opening '" & Filename & "'" & ControlChars.CrLf & ex.Message, MsgBoxStyle.Exclamation, "Error Opening File")
+            Exit Sub
+        End Try
+
+        Timer1.Enabled = False
+        Select Case FileType
+            'Case 1 ' This is handled in "Case Else" because we default to text
+            Case 2 'CSV File
+                Try
+                    OutFile.WriteLine("PID, ""Title"", ""Process Name"", ""Command Line""")
+                    For Each LI As ListViewItem In lsvWindows.Items
+                        OutFile.WriteLine("{0}, ""{1}"", ""{2}"", ""{3}""", LI.SubItems(0).Text, LI.SubItems(1).Text, LI.SubItems(2).Text, LI.SubItems(3).Text)
+                    Next
+                Catch ex As Exception
+                    MsgBox("Error writing to file: " & ex.Message, MsgBoxStyle.Exclamation, "Error Writing to File")
+                Finally
+                    OutFile.Close()
+                End Try
+            Case 3 'XML File
+                Try
+                    Dim MyWriter As Xml.XmlWriter = Xml.XmlWriter.Create(OutFile)
+                    MyWriter.WriteStartDocument(True)
+                    MyWriter.WriteStartElement("processes")
+
+                    For Each LI As ListViewItem In lsvWindows.Items
+                        MyWriter.WriteStartElement("process")
+                        MyWriter.WriteElementString("pid", LI.SubItems(0).Text)
+                        MyWriter.WriteElementString("title", LI.SubItems(1).Text)
+                        MyWriter.WriteElementString("name", LI.SubItems(2).Text)
+                        MyWriter.WriteElementString("commandline", LI.SubItems(3).Text)
+                        MyWriter.WriteEndElement()
+                    Next
+
+                    MyWriter.WriteEndElement()
+                    MyWriter.WriteEndDocument()
+                    MyWriter.Close()
+                Catch ex As Exception
+                    MsgBox("Error writing to file: " & ex.Message, MsgBoxStyle.Exclamation, "Error Writing to File")
+                Finally
+                    OutFile.Close()
+                End Try
+
+            Case Else 'Unknown type, default to text
+                Try
+                    OutFile.WriteLine("PID  Title   Process Name    Command Line")
+                    For Each LI As ListViewItem In lsvWindows.Items
+                        OutFile.WriteLine("{0}  {1} {2} {3}", LI.SubItems(0).Text, LI.SubItems(1).Text, LI.SubItems(2).Text, LI.SubItems(3).Text)
+                    Next
+                Catch ex As Exception
+                    MsgBox("Error writing to file: " & ex.Message, MsgBoxStyle.Exclamation, "Error Writing to File")
+                Finally
+                    OutFile.Close()
+                End Try
+        End Select
+        Timer1.Enabled = tsbMonitoring.Checked
+    End Sub
+
+    Private Sub tsbMonitoring_CheckedChanged(sender As Object, e As EventArgs) Handles tsbMonitoring.CheckedChanged
+        Timer1.Enabled = tsbMonitoring.Checked
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Console.WriteLine(System.Runtime.InteropServices.Marshal.GetTypeLibGuidForAssembly(System.Reflection.Assembly.GetExecutingAssembly).ToString)
+        Console.WriteLine(CType(Me.GetType.Assembly.GetCustomAttributes(GetType(System.Runtime.InteropServices.GuidAttribute), False)(0), System.Runtime.InteropServices.GuidAttribute).Value.ToString)
+
+    End Sub
+End Class
